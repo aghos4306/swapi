437387e67aec7eb6968d16cec4861d1ed7f6458d
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-05-16 01:44:54 +0300

    fetch api data in context

--- src/context.js
+++ src/context.js
@@ -2,12 +2,38 @@ import React, { useState, useEffect, useContext } from 'react'
 
 export const API_ENDPOINT = `https://swapi.dev/api/starships/`
 
+console.log(API_ENDPOINT)
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState({ show: false, msg: '' })
+    const [swapiContent, setSwapiContent] = useState([])
+    const [query, setQuery] = useState('Iron Man')
+
+    const fetchSwapiContent = async (url) => {
+        setIsLoading(true)
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+            //console.log(data.results[0].name)
+            setSwapiContent(data.results)
+            setError({ show: false, msg: '' })
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error)
+            setError({ show: true, msg: 'Error loading data...' })
+        }
+        
+    }
+
+    useEffect(() => {
+        fetchSwapiContent(`${API_ENDPOINT}`)
+    }, [])
     
     return (
-        <AppContext.Provider value="hello">
+        <AppContext.Provider value={{isLoading, error, query, swapiContent, setQuery}}>
           {children}
         </AppContext.Provider>
     )
